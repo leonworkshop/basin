95a06e6c10f57ba964dd3fe56608a9b2b99a5b27
Leon Cui <leon.cui@outlook.com>
2014-09-16 12:26:09 +0800

    changed the virtualenv installation
    Signed-off-by: Leon Cui <leon.cui@outlook.com>

--- sichuan/puppet/modules/meili/manifests/install.pp
+++ sichuan/puppet/modules/meili/manifests/install.pp
@@ -7,17 +7,29 @@ class meili::install inherits meili {
   $rootdir = "${root_dir}"
 
   # setup virtualenv
-  exec { 'setup-virtualenv':
-    command => "python ${tools_path}/install_venv.py",
-    path => ['/bin', '/sbin', '/usr/bin'],
+  python::virtualenv { '/opt/meili':
+      ensure => present,
+      version => 'system',
+      requirements => '/opt/meili/requirements.txt',
+      venv_dir => '/opt/meili/.venv',
+      cwd => '/opt/meili',
+      owner => "${user}",
+      group => "${group}",
+      timeout => 300
   }
+
+#  exec { 'setup-virtualenv':
+#    command => "python ${tools_path}/install_venv.py",
+#    path => ['/bin', '/sbin', '/usr/bin'],
+#  }
 
   # setup meili cli environment
   exec { 'setup-meili-develop':
     command => "${tools_path}/with_venv.sh python setup.py develop",
     path => ['/bin', '/sbin', '/usr/bin', '/usr/local/bin'],
     cwd => "${root_dir}",
-    require => Exec['setup-virtualenv'],
+#    require => Exec['setup-virtualenv'],
+    require => Python::Virtualenv['/opt/meili'],
   }
 
   file { "${tools_path}/with_venv.sh":

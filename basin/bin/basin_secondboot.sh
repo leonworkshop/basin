3511ad3f92cc26460026dddfd7313eb323bcd877
$github_user <$github_user_email>
2014-10-10 09:07:04 +0800

    A few changes for redis integration
    Signed-off-by: $github_user <$github_user_email>

--- basin/bin/basin_secondboot.sh
+++ basin/bin/basin_secondboot.sh
@@ -50,6 +50,33 @@ export FACTER_github_user_email=$GITHUB_USER_EMAIL
 puppet_with_retry "/root/secondboot.pp"
 print_msg "puppet second bootstrap is done"
 
+# install credis lib
+cd /tmp/
+wget http://credis.googlecode.com/files/credis-0.2.3.tar.gz
+tar xzf credis*gz
+cd credis*
+cat > /tmp/patch.credis << EOP
+--- ../credis_2/credis-0.2.3/credis.c   2010-08-27 01:57:25.000000000 -0700
++++ ../credis-0.2.3/credis.c    2014-01-28 22:39:42.000000000 -0800
+@@ -754,7 +754,7 @@
+    * first 1.1.0 release(?), e.g. stable releases 1.02 and 1.2.6 */
+   if (cr_sendfandreceive(rhnd, CR_BULK, "INFO\r\n") == 0) {
+     int items = sscanf(rhnd->reply.bulk,
+-                       "redis_version:%d.%d.%d\r\n",
++                       "# Server\r\nredis_version:%d.%d.%d\r\n",
+                        &(rhnd->version.major),
+                        &(rhnd->version.minor),
+                        &(rhnd->version.patch));
+EOP
+# see the patch lower down https://code.google.com/p/credis/wiki/Examples
+patch -p2 < /tmp/patch.credis
+make
+cp *.h /usr/include/
+cp *.so /usr/lib/
+cp *.a /usr/lib/
+cp libcredis.so /usr/lib/
+
+
 system_ready "Completed secondboot successfully."
 
 echo "----------> apply the every service puppets <----------"

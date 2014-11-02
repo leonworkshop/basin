87578897b49601a1222203b0539667aecad0246d
Leon Cui <leon.cui@outlook.com>
2014-11-02 13:40:26 +0800

    basin: modified meili-puppet module to enable meili service automatically
    Signed-off-by: Leon Cui <leon.cui@outlook.com>

--- sichuan/puppet/manifests/test.pp
+++ sichuan/puppet/manifests/test.pp
@@ -5,12 +5,12 @@ class { 'nginx':
   proxy_send_timeout => '5s',
 }
 
-nginx::resource::vhost { 'api.shucaibao.com':
+nginx::resource::vhost { 'api.shoowo.com':
     ensure => 'present',
     ssl => true,
     ssl_port => '443',
-    ssl_cert => "/opt/shucaibao/certs/shucaibao.com.crt",
-    ssl_key => "/opt/shucaibao/certs/shucaibao.com.key",
+    ssl_cert => "/opt/shoowo/certs/shoowo.com.crt",
+    ssl_key => "/opt/shoowo/certs/shoowo.com.key",
     listen_ip => '115.28.40.5',
     listen_port => '8088',
     proxy_set_header => [
@@ -28,7 +28,7 @@ nginx::resource::vhost { 'api.shucaibao.com':
 
 nginx::resource::location { '/api/':
     ensure => 'present',
-    vhost => "api.shucaibao.com",
+    vhost => "api.shoowo.com",
     ssl => true,
     ssl_only => true,
     proxy => 'http://127.0.0.1:9000/api/',
@@ -36,7 +36,7 @@ nginx::resource::location { '/api/':
 
 nginx::resource::location { '/status':
     ensure => 'present',
-    vhost => "api.shucaibao.com",
+    vhost => "api.shoowo.com",
     stub_status => true,
 #    location_allow   => ['127.0.0.1'],
 #    location_deny    => ['all'],
@@ -61,46 +61,11 @@ class { '::meili':
   aliyun_access_key => 'm8UQmEhpnLni9z6G6yAIFNWdT8Sh21',
   ots_instance_name =>'EMEIALIHZOTS0002',
   ots_instance_region =>'cn-hangzhou',
-  oss_instance_name =>'alihzoss0002',
-  oss_instance_region =>'cn-hangzhou',
+  oss_instances =>[ {'name' => 'alihzoss0001', 'region' => 'cn-hangzhou' },
+                    {'name' => 'alihzoss0002', 'region' => 'cn-hangzhou' } ],
   mqs_instance_owner_id =>'e2x86rc97q',
   mqs_instance_name =>'ALIHZMQS0002',
   mqs_instance_region =>'cn-hangzhou',
-}
-
-class { '::shennv':
-    service_ensure => 'present',
-    service_enable => true,
-    mode => 'all',
-    redis_host => '127.0.0.1',
-    redis_port => 6379,
-    rds_host => 'rdsqbmi7za6bzv2.mysql.rds.aliyuncs.com',
-    aliyun_access_id => 'rc7nop4ytXKsi0Kn',
-    aliyun_access_key => 'm8UQmEhpnLni9z6G6yAIFNWdT8Sh21',
-    ots_instance_name =>'EMEIALIHZOTS0002',
-    ots_instance_region =>'cn-hangzhou',
-    oss_instance_name =>'alihzoss0002',
-    oss_instance_region =>'cn-hangzhou',
-    mqs_instance_owner_id =>'e2x86rc97q',
-    mqs_instance_name =>'ALIHZMQS0002',
-    mqs_instance_region =>'cn-hangzhou',
-}
-
-class { '::gongga':
-    service_ensure => 'present',
-    service_enable => true,
-    redis_host => '127.0.0.1',
-    redis_port => 6379,
-    rds_host => 'rdsqbmi7za6bzv2.mysql.rds.aliyuncs.com',
-    aliyun_access_id => 'rc7nop4ytXKsi0Kn',
-    aliyun_access_key => 'm8UQmEhpnLni9z6G6yAIFNWdT8Sh21',
-    ots_instance_name =>'EMEIALIHZOTS0002',
-    ots_instance_region =>'cn-hangzhou',
-    oss_instance_name =>'alihzoss0002',
-    oss_instance_region =>'cn-hangzhou',
-    mqs_instance_owner_id =>'e2x86rc97q',
-    mqs_instance_name =>'ALIHZMQS0002',
-    mqs_instance_region =>'cn-hangzhou',
 }
 
 

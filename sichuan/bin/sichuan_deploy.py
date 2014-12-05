c51f998cfa517ba10c9d73870a3e3effab663167
Leon Cui <leon.cui@outlook.com>
2014-12-05 10:35:22 +0800

    Fixed a working directory issue
    Signed-off-by: Leon Cui <leon.cui@outlook.com>

--- sichuan/bin/sichuan_deploy.py
+++ sichuan/bin/sichuan_deploy.py
@@ -39,6 +39,8 @@ def parseArguments():
     parser.add_argument('--pbt',
                         default='/opt/shoowo/skeleton/xmt/phase_build.yaml',
                         help="Phase build table file path")
+    parser.add_argument('--shoowo_dir',
+                        default='/opt/shoowo/shoowo')
     args = parser.parse_args()
     return args
 
@@ -73,7 +75,7 @@ def load_yaml(yaml_path):
     return yaml_data
 
 
-def deploy_build(pmt_entry, pbt):
+def deploy_build(args, pmt_entry, pbt):
     """
     Deploy the build specified in pbt table
     """
@@ -84,14 +86,14 @@ def deploy_build(pmt_entry, pbt):
 
     # checkout the branch
     d_branch = pbt[phase]['branch']
-    utils.run_command("git checkout " + d_branch)
+    utils.run_command("cd " + args.shoowo_dir + ";git checkout " + d_branch)
 
     # if tag is speicifed, checkout that tag
     if 'tag' in pbt[phase]:
-        utils.run_command("git reset --hard " + pbt[phase]['tag'])
+        utils.run_command("cd " + args.shoowo_dir + ";git reset --hard " + pbt[phase]['tag'])
         log.info(" -- deployed the build at TAG %s", pbt[phase]['tag'])
     else:
-        utils.run_command('git pull')
+        utils.run_command('cd ' + args.shoowo_dir + ';git pull')
         log.info(" -- deployed the lastest build")
 
 
@@ -115,7 +117,7 @@ def main():
 
         # start deployment with the specified build
         pbt = load_yaml(args.pbt)
-        deploy_build(pmt_entry, pbt)
+        deploy_build(args, pmt_entry, pbt)
 
     except Exception as e:
         log.error("Error happens during post boot: %s", e)
